7cc996c36b82b2c33a7eedd94cf019ab36e41940
pontakornth <pontakorn61@gmail.com>
2023-05-18 19:22:00 +0700

    remove unused layout
    I only have one layout

--- app.vue
+++ app.vue
@@ -1,7 +1,16 @@
+<script setup lang="ts">
+import { useTodosStore } from '~/stores/todosStore';
+const todosStore = useTodosStore()
+</script>
 <template>
   <div>
-    <NuxtLayout>
+    <nav class="p-4 bg-amber-500 flex items-center justify-end gap-8 text-white">
+      <NuxtLink to="/">Home
+
+        <UBadge v-if="todosStore.incompleteTodos.length > 0">{{ todosStore.incompleteTodos.length }}</UBadge>
+      </NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+    </nav>
     <NuxtPage />
-    </NuxtLayout>
   </div>
 </template>
