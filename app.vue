cce5b6759d434aa2ed772ab45d66c2ecef16b53e
pontakornth <pontakorn61@gmail.com>
2023-05-18 19:29:33 +0700

    Adjust dark mode color

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@ const todosStore = useTodosStore()
 </script>
 <template>
   <div>
-    <nav class="p-4 bg-amber-500 flex items-center justify-end gap-8 text-white">
+    <nav class="p-4 bg-amber-500 dark:bg-amber-700 flex items-center justify-end gap-8 text-white">
       <NuxtLink to="/">Home
 
         <UBadge v-if="todosStore.incompleteTodos.length > 0">{{ todosStore.incompleteTodos.length }}</UBadge>
